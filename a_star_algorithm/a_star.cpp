bd646fcd629014a8b238e6a3f336d02d037b5e9c
Deepak Karishetti <r10.deepak@gmail.com>
2019-06-20 21:25:10 -0600

    Print the gameboard to the screen

--- a_star_algorithm/a_star.cpp
+++ a_star_algorithm/a_star.cpp
@@ -4,6 +4,31 @@
 using std::vector;
 using std::cout;
 
+void PrintBoard(vector<vector<int>> board)
+{
+	for (i : board)
+	{
+		for (int j : i)
+		{
+			cout << j << " ";
+		}
+		cout << "\n";
+	}
+}
+/* 
+// Alternate way to do the above using index reference
+void PrintBoard(const vector<vector<int>> board)
+{
+	for (int i = 0; i < board.size(); i++)
+	{
+		for (int j = 0; j < board[i].size(); j++)
+		{
+			cout << board[i][j];
+		}
+		cout << "\n";
+	}
+}
+*/
 int main()
 {
 	// Declaring the board variable
@@ -13,4 +38,6 @@ int main()
 		 {0, 1, 0, 0, 0, 0},
 		 {0, 1, 0, 0, 0, 0},
 		 {0, 0, 0, 0, 1, 0}};
+
+	PrintBoard(board);
 }
